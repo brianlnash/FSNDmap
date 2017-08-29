3d7852adb8b5d853b9fdde669d487518af000a30
Brian Nash <brianlnash@gmail.com>
2017-08-29 10:30:20 -0700

    updated Smith's Barcadere (quote escaping)

--- js/locations.js
+++ js/locations.js
@@ -2,28 +2,28 @@
 
 var snorkelList = [
     {
-    name: "Cemetery Beach",
-    lat: 19.3656096,
-    lng: -81.3953804,
+        name: 'Cemetery Beach',
+        lat: 19.3656096,
+        lng: -81.3953804
     },
     {
-    name: "Cheeseburger Reef",
-    lat: 19.3035886,
-    lng: -81.3849887,
+        name: 'Cheeseburger Reef',
+        lat: 19.3035886,
+        lng: -81.3849887
     },
     {
-    name: "Rum Point Beach",
-    lat: 19.37195,
-    lng: -81.27155,
+        name: 'Rum Point Beach',
+        lat: 19.37195,
+        lng: -81.27155
     },
     {
-    name: "Smith's Barcadere",
-    lat: 19.276628,
-    lng: -81.390988,
+        name: 'Smith\'s Barcadere',
+        lat: 19.276628,
+        lng: -81.390988
     },
     {
-    name: "Turtle Reef",
-    lat: 19.3824723,
-    lng: -81.4166321,
-    },
+        name: 'Turtle Reef',
+        lat: 19.3824723,
+        lng: -81.4166321
+    }
 ];
